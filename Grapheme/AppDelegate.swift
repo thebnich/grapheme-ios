9cefe978149aa81e729c48b6484dcc83b8df7e31
Brian Nicholson <thebnich@gmail.com>
2016-06-23 10:54:45 -0700

    initial project

---
+++ Grapheme/AppDelegate.swift
@@ -0,0 +1,31 @@
+/* This Source Code Form is subject to the terms of the Mozilla Public
+ * License, v. 2.0. If a copy of the MPL was not distributed with this
+ * file, You can obtain one at http://mozilla.org/MPL/2.0/. */
+
+import UIKit
+
+@UIApplicationMain
+class AppDelegate: UIResponder, UIApplicationDelegate {
+
+    var window: UIWindow?
+
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
+        self.window = UIWindow(frame: UIScreen.mainScreen().bounds)
+        self.window?.backgroundColor = UIColor.whiteColor()
+        self.window!.rootViewController = ViewController()
+        self.window!.makeKeyAndVisible()
+
+        return true
+    }
+
+    func applicationWillResignActive(application: UIApplication) {}
+
+    func applicationDidEnterBackground(application: UIApplication) {}
+
+    func applicationWillEnterForeground(application: UIApplication) {}
+
+    func applicationDidBecomeActive(application: UIApplication) {}
+
+    func applicationWillTerminate(application: UIApplication) {}
+}
+
